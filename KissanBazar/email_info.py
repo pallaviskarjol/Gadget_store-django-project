25cb5c39a8240ce3c2d1a76741b7cf0366c38134
pallaviskarjol <pallaviskarjol@gmail.com>
2019-07-13 11:47:00 +0530

    first project

---
+++ KissanBazar/email_info.py
@@ -0,0 +1,6 @@
+
+EMAIL_USE_TLS=True
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_HOST_USER="pallaviskarjol@gmail.com"
+EMAIL_HOST_PASSWORD="147852369"
+EMAIL_PORT=587
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn KissanBazar.wsgi --log-file -
